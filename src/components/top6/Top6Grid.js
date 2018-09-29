6e148c45335ce3ddaade91e3efa5b6d175c03750
Ryan McNierney <ryan.mcnierney@gmail.com>
2018-09-29 11:13:20 -0400

    refresh button works

--- src/components/top6/Top6Grid.js
+++ src/components/top6/Top6Grid.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { top6Fetch } from '../../reducers/top6Reducer'
-import Loading from '../Loading'
 import '../styles/top6.css'
 
 class Top6Grid extends Component {
@@ -16,58 +15,45 @@ class Top6Grid extends Component {
   }
 
   render() {
-    const { top6, top6Loading } = this.props
-
+    const { top6 } = this.props
     return (
       <div id="top6-content">
-        {
-          (top6Loading === true) ? <Loading /> :
-            <React.Fragment>
-              <div id="top6-top">
-                <div id="top6-title">
-                  <h1>Top 6</h1>
-                </div>
-                <div id="refresh">
-                  <button id="refresh" onClick={this.handleClick}>Refresh Data</button>
-                </div>
-              </div>
-              <div id="top6-grid">
-                <table>
-                  <thead>
-                    <tr id="top6-row">
-                      <th id="top6-head">Rank</th>
-                      <th id="top6-head">Team</th>
-                      <th id="top6-head">Proj</th>
-                      <th id="top6-head">Actual</th>
+        <div id="top6-top">
+          <div id="top6-title">
+            <h1>Top 6</h1>
+          </div>
+          <div id="refresh">
+            <button id="refresh" onClick={this.handleClick}>Refresh Data</button>
+          </div>
+        </div>
+        <div id="top6-grid">
+          <table>
+            <thead>
+              <tr id="top6-row">
+                <th id="top6-head">Rank</th>
+                <th id="top6-head">Team</th>
+                <th id="top6-head">Proj</th>
+                <th id="top6-head">Actual</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                top6.map(team => {
+                  return (
+                    <tr key={team.teamIdx} id="top6-row">
+                      <td>{team.rank}</td>
+                      <td id="team-name"><img alt="team-icon" id="table-icon" src={team.img} /><span>{team.team}</span></td>
+                      <td>{team.projected}</td>
+                      <td>{team.actual}</td>
                     </tr>
-                  </thead>
-                  <tbody>
-                    {
-                      top6.map(team => {
-                        return (
-                          <tr key={team.teamIdx} id="top6-row">
-                            <td>{team.rank}</td>
-                            <td id="team-name"><img alt="team-icon" id="table-icon" src={team.img} /><span>{team.team}</span></td>
-                            <td>{team.projected}</td>
-                            <td>{team.actual}</td>
-                          </tr>
-                        )
-                      })
-                    }
-                  </tbody>
-                </table>
-              </div>
-            </React.Fragment>
-        }
+                  )
+                })
+              }
+            </tbody>
+          </table>
+        </div>
       </div>
     )
-  }
-}
-
-const mapState = state => {
-  return {
-    top6Store: state.top6.top6,
-    top6Loading: state.top6.loading
   }
 }
 
@@ -77,4 +63,4 @@ const mapProps = dispatch => {
   }
 }
 
-export default connect(mapState, mapProps)(Top6Grid)
+export default connect(null, mapProps)(Top6Grid)
